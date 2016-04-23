b5443a407a436eca52d2ba85eb4c237ba13d1da8
vmanisha <manisha.verma@research.iiit.ac.in>
2016-04-24 00:12:10 +0100

    Start search result serving

--- server/search_manager.js
+++ server/search_manager.js
@@ -1,12 +1,32 @@
-
 // 1. Fetch the results from Microsoft API
 // 2. Display the results from Fedweb. 
 // ----------------------------------------
 
 var VerticalEnum = Object.freeze({IMAGE: 0, VIDEO: 1, WIKI: 2, OTHER:3});
- 
+
+// Queue that manages first result. 
+first_result_queues = {}
+
 
 // Generate the results for a single query.
+module.exports = {
+
+	searchQuery: function(user_name, task_id, pref_order, query_text) {
+		if (!(task_id in first_result_queues))
+		{
+		  // this task has not been assigned before. 
+		  // Take the preferences and randomly permute the list.
+
+
+		}
+
+
+	}
+
+
+
+};
+
 
 
 // Set up the 
