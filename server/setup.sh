d8e849d073691316ae4cb786ee06104902fed979
mverma <mverma@mobiletasks.cs.ucl.ac.uk>
2016-04-28 03:23:13 +0100

    Adding setup.sh

---
+++ server/setup.sh
@@ -0,0 +1,12 @@
+npm install nodejs-microdb
+npm install express
+npm install mustache
+npm install consolidate
+npm install body-parser
+npm install base-64
+npm install change-case
+npm install ejs
+npm install sync-request
+npm install mathjs
+
+
