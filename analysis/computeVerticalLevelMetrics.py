ed4a331259fa019165faf2a0ef9bfc6d4443f334
vmanisha <manisha.verma@research.iiit.ac.in>
2016-07-27 17:43:42 +0100

    Added classification and ROC plot

--- analysis/computeVerticalLevelMetrics.py
+++ analysis/computeVerticalLevelMetrics.py
@@ -7,25 +7,116 @@ from scipy.stats.mstats import mannwhitneyu, kruskalwallis,ttest_ind
 from plotStats import *
 import math
 
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.metrics import roc_curve, auc, confusion_matrix
+from scipy import interp
+import matplotlib.pyplot as plt
+
+
 TASKSATMAP = {'Somewhat Satisfied': 2.0, 'Highly Satisfied': 3.0, 'Not Satisfied' : 1.0}
 
 def CorrelationAndClassification(features):
     print features.columns
+    features = features.reset_index()
     # get satisfaction and relevance labels 
-    satisfaction = features['satisfaction']
+    satisfaction = features['satisfaction'].map({1:0,2:0, 3:0, 4:1,5:1 })
+    print satisfaction.value_counts()
+    print satisfaction.shape
     relevance = features['relevance']
 
     # take every column and find 
     # set all other values to 0. 
-    remaining_features = features.drop([\
-        # 'relevance','satisfaction',\
-        'first_event_time','first_result_time','query_task'], axis =1)
+    remaining_features = features.drop(['relevance','satisfaction',\
+            'first_event_time','first_result_time','query_task'], axis =1)
     remaining_features = remaining_features.fillna(0)
-    print remaining_features.columns
 
     # write to another file.
     remaining_features.to_csv('serp_features_clean.csv',index=False,encoding='utf-8')
+    # map the vertical name to number.
+    remaining_features['result_type'] =\
+            remaining_features['result_type'].map({'i':1, 'v':2, 'w':3, 'o':4})
+    
+    # Merge the features : (tap and double tap)
+    remaining_features['tap_count'] += remaining_features['doubletap_count']
+    remaining_features['tap_distance'] +=remaining_features['doubletap_distance']
+    remaining_features['first_tap_time'] = \
+            remaining_features[['first_doubletap_time',\
+            'first_tap_time']].min(axis=1)
+    remaining_features =  remaining_features.drop(['doubletap_count',\
+                                'doubletap_distance','first_doubletap_time','index'],
+                                axis = 1)
+ 
 
+    # Normalize the features
+    remaining_features = (remaining_features - remaining_features.mean()) \
+            / (remaining_features.max() - remaining_features.min())
+    # Convert the features into lists. 
+    features_list = remaining_features.values.tolist()
+   
+    for c in [0.01, 0.1,1,5,10,100]:
+        mean_tpr = 0.0
+        mean_fpr = np.linspace(0, 1, 100)
+        all_tpr = []
+        confusion_matrix_total = {}
+        # Predict Satisfaction using Logistic regression and print auc.
+        for k, (train, test) in enumerate(StratifiedKFold(satisfaction, 5)):
+            x_train = [ features_list[i] for i in train]
+            y_train = satisfaction.loc[train]
+            x_test = [ features_list[i] for i in test]
+            y_test = satisfaction[test]
+            clf = LogisticRegression(C=c, penalty='l1')
+            clf_fit = clf.fit(x_train, y_train)
+            coeff = clf_fit.coef_.ravel()
+            coeff_tuple = []
+            for i in range(len(list(remaining_features.columns))):
+                coeff_tuple.append((remaining_features.columns[i],round(coeff[i],5)))
+            print 'k=',k,'c=',c
+            for ctuple in coeff_tuple:
+                print ctuple[0], ctuple[1]
+            # Get the confusion matrices. 
+            y_predict= clf_fit.predict(x_test)
+            confusion_fold = confusion_matrix(y_test, y_predict)
+            for i in range(len(confusion_fold)):
+                total = sum(confusion_fold[i])
+                if i not in confusion_matrix_total:
+                    confusion_matrix_total[i] = {}
+                for j in range(len(confusion_fold)):
+                    if j not in confusion_matrix_total[i]:
+                        confusion_matrix_total[i][j] = 0.0
+                    confusion_matrix_total[i][j] += (confusion_fold[i][j] *1.0)/total 
+            
+            prob_y_test = clf_fit.predict_proba(x_test)
+            fpr, tpr, thresholds = roc_curve(y_test, prob_y_test[:, 1])
+            mean_tpr += interp(mean_fpr, fpr, tpr)
+            mean_tpr[0] = 0.0
+            roc_auc = auc(fpr, tpr)
+            plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (k,
+                roc_auc))
+
+        for i, jdict in confusion_matrix_total.items():
+            for j,value in jdict.items():
+                print 'logistic c=',c,i,j,value/5
+
+
+        plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Random')
+        mean_tpr /= 5  # Number of folds. 
+        mean_tpr[-1] = 1.0
+        mean_auc = auc(mean_fpr, mean_tpr)
+        plt.plot(mean_fpr, mean_tpr, 'k--',
+                         label='Mean ROC (area = %0.2f)' % mean_auc, lw=2)
+
+        plt.xlim([-0.05, 1.05])
+        plt.ylim([-0.05, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic Logistic C='+str(c))
+        #plt.legend(loc="lower right")
+        plt.legend(loc='lower right', ncol = 1, fontsize=15)
+        plt.savefig('roc_logistic_'+str(c)+'.png',bbox_inches='tight')
+        plt.show()
+        #plt.clf()
 
 def ComputeSERPFeatures(merged_table):
     grouped_table = merged_table.groupby(['user_id','task_id'])
@@ -72,8 +163,15 @@ def ComputeSERPFeatures(merged_table):
                         row['time'])
 
             if query_result_type_string:
-
                 if row['type'] == 'event':
+                    # Set last event time. 
+                    if 'last_event_time' in\
+                        serp_score_and_features[query_result_type_string]:
+                        serp_score_and_features[query_result_type_string]['last_event_time']=\
+                            max(serp_score_and_features[query_result_type_string]['last_event_time'],row['time'])
+                    else:
+                        serp_score_and_features[query_result_type_string]['last_event_time']=row['time']
+
                     # Add first event time. 
                     event_type = row['event_type']
                     serp_score_and_features[query_result_type_string] = SetSerpFeature(\
@@ -164,10 +262,13 @@ def ComputeSERPFeatures(merged_table):
                             if key in entry:
                                 serp_score_and_features[entry][row['response_type']]=\
                                         row['response_value']
-                                serp_score_and_features[query_result_type_string]=\
-                                        SetSerpFeature('last_event_time',\
-                                        serp_score_and_features[query_result_type_string],\
-                                        row['time'])
+                                # Set last event time. 
+                                if 'last_event_time' in\
+                                    serp_score_and_features[query_result_type_string]:
+                                        serp_score_and_features[query_result_type_string]['last_event_time']=\
+                                        max(serp_score_and_features[query_result_type_string]['last_event_time'],row['time'])
+                                else:
+                                    serp_score_and_features[query_result_type_string]['last_event_time']=row['time']
         # Update the data for last serp in group. 
         if query_result_type_string:
             if 'last_event_time' not in\
@@ -180,7 +281,6 @@ def ComputeSERPFeatures(merged_table):
     # Use the dictionary to create training data.
     serp_feat_list = []
     for key, feat_dict in serp_score_and_features.items():
-        print key, feat_dict 
         feat_dict['query_task'] = key[key.find('_')+1:]
         feat_dict['result_type'] = key[:key.find('_')]
         # get the time differences in second and delete the timestamps. 
@@ -192,17 +292,26 @@ def ComputeSERPFeatures(merged_table):
         for feat_key in feat_dict.keys():  
             if 'time' in feat_key:
                 feat_dict[feat_key] = (feat_dict[feat_key] - first_time)\
-                                                        .total_seconds()
+                        .total_seconds()
+        for feat_key in feat_dict.keys():  
+            if 'viewport_ms' in feat_key:
+                # Convert ms into seconds and then convert into ratio.  
+                feat_dict[feat_key] =  (feat_dict[feat_key]*0.001)
+
         serp_feat_list.append(feat_dict)
 
     serp_feature_frame = pd.DataFrame.from_dict(serp_feat_list)
     filtered_feature_frame = serp_feature_frame[\
                     ~(serp_feature_frame['relevance'].isnull() |\
                     serp_feature_frame['relevance'].isnull())]
-    
+    filtered_feature_frame = filtered_feature_frame[\
+            filtered_feature_frame['last_event_time'] < 500.0]
+   
+    '''
     PlotSatAndRelBoxPlotPerVertical(filtered_feature_frame.groupby('result_type'),\
                                     'SERP Response',\
                                     'rel_sat_serp_resonse.png')
+    '''
     return filtered_feature_frame
 
 
