27ebde951170688ec0c6ef6209814d1b9423fdc4
vmanisha <manisha.verma@research.iiit.ac.in>
2016-10-30 17:14:13 +0000

    Adding costs

---
+++ costs/costAnalysis.py
@@ -0,0 +1,27 @@
+# Find the following costs - satisfaction associations and parameter values.
+
+# Number of words -- Satisfaction 
+
+def QueryCostAndBenefitAnalysis(merged_table):
+    # For each query input, get satisfaction
+    query_time_sat_table = merged_table[['query','time','']]
+
+    
+
+
+
+# Cost of querying 
+# Cost of assessing 
+# Number of documents analysed 
+# cost of clicking
+def DocumentCostBenefitAnalysis():
+
+
+
+
+def ScrollClickCostAnalysis():
+
+
+
+def DocumentCostGainAnalysis():
+
